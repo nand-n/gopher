bf00cfe161995fd0ac762fc811da8ed9b6380ef7
Nahi002 <nahomdebele002@gmail.com>
2024-06-25 21:31:14 +0300

    added the hands on exercise 24

--- handson-exercise24/main.go
+++ handson-exercise24/main.go
@@ -5,10 +5,22 @@ import "fmt"
 /****
 Create a program that has a looop that prints every number form 0 to 99
 Modify the program  to run 100 curriculum item
+
+Create a data stucture called a map
+
 */
 
 func main() {
-	for i := 0; i < 100; i++ {
-		fmt.Println("number : ", i)
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Println("number : ", i)
+	// }
+
+	m := map[string]int{
+		"james": 43,
+		"Money": 43,
+	}
+
+	for k, v := range m {
+		fmt.Printf("key %v \t vlaues %v \n", k, v)
 	}
 }
