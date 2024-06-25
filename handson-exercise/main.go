89a0f511d87accd54e30dfcc6d3d4dc15fc07a46
Nahi002 <nahomdebele002@gmail.com>
2024-06-25 20:42:33 +0300

    added a swith statment

--- handson-exercise/main.go
+++ handson-exercise/main.go
@@ -13,5 +13,20 @@ func main() {
 		fmt.Println("less than 100")
 	} else if x >= 101 && x <= 200 {
 		fmt.Println("between 101 and 200")
+	} else if x >= 201 && x <= 250 {
+		fmt.Println("between 201 and 250")
+	} else {
+		fmt.Println("This was more than 250")
+	}
+
+	switch {
+	case x <= 100:
+		fmt.Println(("Less than 100"))
+	case x >= 101 && x <= 200:
+		fmt.Println("between 101 and 200")
+	case x >= 201 && x <= 250:
+		fmt.Println("between 201 and 250")
+	default:
+		fmt.Println("this was more than 250")
 	}
 }
