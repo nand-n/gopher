852cad69be781676118c6f805d666b90bbdc5b58
Nahi002 <nahomdebele002@gmail.com>
2024-06-25 21:27:49 +0300

    added the hands on exercise 24

---
+++ handson-exercise24/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/****
+Create a program that has a looop that prints every number form 0 to 99
+Modify the program  to run 100 curriculum item
+*/
+
+func main() {
+	for i := 0; i < 100; i++ {
+		fmt.Println("number : ", i)
+	}
+}
