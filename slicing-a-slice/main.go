8efd8cb1304efad5bd9b1f46b37f7789f030af6f
Nahi002 <nahomdebele002@gmail.com>
2024-06-30 11:25:25 +0300

    added hands on exersises and array literals

--- slicing-a-slice/main.go
+++ slicing-a-slice/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	xs := []int{44, 45, 46, 47, 48, 49, 50}
@@ -20,4 +23,32 @@ func main() {
 			fmt.Printf("Element at [%d][%d]: %d\n", i, j, matrix[i][j])
 		}
 	}
+
+	n := []float64{3, 1, 4, 2}
+
+	fmt.Println(medianOne(n))
+	fmt.Println(medianTwo(n))
+
+}
+
+func medianOne(x []float64) float64 {
+	sort.Float64s(x)
+	i := len(x) / 2
+	if len(x)%2 == 1 {
+		return x[i/2]
+	}
+	return (x[i-1] + x[i]/2)
+}
+
+func medianTwo(x []float64) float64 {
+	//allocate a new underlying array
+	n := make([]float64, len(x))
+	copy(n, x)
+	sort.Float64s(n)
+	i := len(n) / 2
+	if len(n)%2 == 1 {
+		return n[i/2]
+	}
+	return (n[i-1] + n[i]/2)
+
 }
