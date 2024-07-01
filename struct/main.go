179980cdfaa0bff3fc2c2cdb03af98b37b19224a
Nahi002 <nahomdebele002@gmail.com>
2024-07-01 23:00:51 +0300

    added code of embaded structs

---
+++ struct/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+			age:   22,
+		},
+		ltk: true,
+	}
+
+	fmt.Println(sa1)
+}
