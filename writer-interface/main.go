4d217247fded8dea3043e2b7504dcf1c09ba5493
Nahi002 <nahomdebele002@gmail.com>
2024-07-02 23:34:10 +0300

    added the go writer interface

---
+++ writer-interface/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type person struct {
+	first string
+}
+
+func (p person) writeOut(w io.Writer) error {
+	_, err := w.Write([]byte(p.first))
+	return err
+}
+
+func main() {
+	f, err := os.Create("Output.txt")
+
+	if err != nil {
+		log.Fatalf("Error %s", err)
+	}
+	defer f.Close()
+
+	s := []byte("Hello pohers!")
+
+	_, err = f.Write(s)
+
+	if err != nil {
+		log.Fatalf("error %s", err)
+	}
+}
