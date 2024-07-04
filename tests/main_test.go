c3e5e8b6a2a49dc579b38defb6a6df083b507b20
Nahi002 <nahomdebele002@gmail.com>
2024-07-04 23:22:18 +0300

    add parameter changed

--- tests/main_test.go
+++ tests/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	total := Add(5, 5)
+	total := add(5, 5)
 	if total != 10 {
 		t.Errorf("Sum was incorrect , got %d , what %d", total, 10)
 	}
