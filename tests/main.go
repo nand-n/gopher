14b880aaf0b4a38658719097c15bcaefa872c3d7
Nahi002 <nahomdebele002@gmail.com>
2024-07-04 23:21:47 +0300

    added the do math adn the subtract fucnn in test main.go

--- tests/main.go
+++ tests/main.go
@@ -4,7 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println(Add(4, 5))
+
+	x := doMath(42, 16, add)
+	fmt.Println(x)
+
+	y := doMath(55, 77, subtract)
+	fmt.Println(y)
 }
-func Add(a int, b int) int {
+func add(a int, b int) int {
 	return a + b
+}
+
+func doMath(a int, b int, f func(int, int) int) int {
+	return f(a, b)
+}
+
+func subtract(a int, b int) int {
+	return a - b
 }
