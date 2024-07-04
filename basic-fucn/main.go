b4654182c49fb4be554d2721c2a2bdcdd7ffcf15
Nahi002 <nahomdebele002@gmail.com>
2024-07-04 22:32:48 +0300

    added basic func with veriadic func argument

--- basic-fucn/main.go
+++ basic-fucn/main.go
@@ -1,8 +1,23 @@
 /*
+exercise 1 :
 Create a func with the identifieer foo that returns anint
 Create a func withthe identifier bar that returns an int and a string
 Call both funcs
 print out their results
+
+exercise 2 :
+Create a func with the identifier foo that
+
+	takes in a veriadic parameter of type int
+	pass in a value of type []int in to your func (unfurl the []int)
+	return the sum of all values of type int passed  ni
+
+Create a func with the identifier bar that
+
+	takes a paramet of type [int]
+	returns the sum of all values of type int passed in
+
+Veriadic function can be called with any number of trailing arguments
 */
 package main
 
@@ -11,6 +26,8 @@ import "fmt"
 func main() {
 	fmt.Println(foo())
 	fmt.Println(bar())
+	xi := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(variadicfunc(xi...))
 }
 
 func foo() int {
@@ -20,4 +37,12 @@ func foo() int {
 
 func bar() (int, string) {
 	return 4, "james Bond"
+}
+
+func variadicfunc(ii ...int) int {
+	t := 0
+	for _, v := range ii {
+		t += v
+	}
+	return t
 }
