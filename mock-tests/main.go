7b2cc847138b060f6469a4a5e4cd8d5875dc06e6
Nahi002 <nahomdebele002@gmail.com>
2024-07-04 23:57:51 +0300

    added Interface of Datastore

--- mock-tests/main.go
+++ mock-tests/main.go
@@ -28,3 +28,16 @@ func (md MockDatastore) SaveUser(u User) error {
 	md.Users[u.ID] = u
 	return nil
 }
+
+// Datastpre defines an interface for storing retrivable data.
+// Any Type that implements this interface (has two methosds) also of type 'Datastore'
+// This means any value of TYPE 'MockDatastore' is also of type Datastore
+//This means we could have a value of type *sql.DB' and it can also be of type 'Datastore '
+//This means we can write functions to take type Datastore and use either of theres :
+// -- 'MockDatastore'
+// -- '*sql.DB'
+
+type Datastore interface {
+	Getuser(id int) (User, error)
+	SaveUser(u User) error
+}
