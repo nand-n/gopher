556e07b0ee6e6766ed4dfb03823826dad652d46d
Nahi002 <nahomdebele002@gmail.com>
2024-07-06 23:51:42 +0300

    added a blocked scope funcion

--- funcreturn/main.go
+++ funcreturn/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func outer() func() int {
 	return func() int {
@@ -18,9 +21,32 @@ func printSquare(f func(int) int, a int) string {
 	return fmt.Sprintf("the number %v squared is %v", a, x)
 }
 
+// blocked scope function \
+func powenator(a float64) func() float64 {
+	var c float64
+	return func() float64 {
+		c++
+		return math.Pow(a, c)
+	}
+}
+
 func main() {
 	f := outer()
 	fmt.Println(f())
 
 	fmt.Println(printSquare(sauare, 3))
+
+	x := powenator(2)
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+	fmt.Println(x())
+
 }
