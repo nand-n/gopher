5164b3c74eacd395440bbaa423809e9a01da2321
Nahi002 <nahomdebele002@gmail.com>
2024-07-06 23:04:53 +0300

    added a code documentation or learned commenting code

--- mcok-test-2/main.go
+++ mcok-test-2/main.go
@@ -6,6 +6,9 @@ func main() {
 	fmt.Println(paradise("Hawaii"))
 }
 
+// The dpcumentation /n
+
+// Paradise  vally
 func paradise(loc string) string {
 	return fmt.Sprint("My idea of paradise is :", loc)
 }
