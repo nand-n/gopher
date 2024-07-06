b3bf08b219811beff449cbf514ee25d0959f344c
Nahi002 <nahomdebele002@gmail.com>
2024-07-06 23:52:37 +0300

    added a blocked scope funcion

--- funcreturn/main.go
+++ funcreturn/main.go
@@ -33,9 +33,7 @@ func powenator(a float64) func() float64 {
 func main() {
 	f := outer()
 	fmt.Println(f())
-
 	fmt.Println(printSquare(sauare, 3))
-
 	x := powenator(2)
 	fmt.Println(x())
 	fmt.Println(x())
