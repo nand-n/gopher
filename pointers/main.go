1a1ebdc55bde43aca827da3d28d6e35369e0e05e
Nahi002 <nahomdebele002@gmail.com>
2024-07-07 23:11:14 +0300

    added pass by reference codes

--- pointers/main.go
+++ pointers/main.go
@@ -6,10 +6,19 @@ func intDelta(n *int) {
 	*n = 43
 }
 
+func mapDelta(md map[string]int, s string) {
+	md[s] = 33
+}
+
 func main() {
 	x := 42
 	fmt.Println(x, "'s memory address is ", &x)
 	intDelta(&x)
 	fmt.Println(x, "'s updated memory address is ", &x)
+	m := make(map[string]int)
+	m["James"] = 32
+	fmt.Println(m["James"])
+	mapDelta(m, "James")
+	fmt.Println(m["James"])
 
 }
