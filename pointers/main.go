3d501fec166a9c03ede54f905ce61593802221ef
Nahi002 <nahomdebele002@gmail.com>
2024-07-07 22:45:41 +0300

    pass by pinter

--- pointers/main.go
+++ pointers/main.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+func intDelta(n *int) {
+	*n = 43
+}
+
 func main() {
 	x := 42
 	fmt.Println(x, "'s memory address is ", &x)
+	intDelta(&x)
+	fmt.Println(x, "'s updated memory address is ", &x)
+
 }
