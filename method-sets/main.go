e70a16edb1d49db4017241e28b6c394635617c07
Nahi002 <nahomdebele002@gmail.com>
2024-07-08 23:35:47 +0300

    method sets

---
+++ method-sets/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type dog struct {
+	first string
+}
+
+func (d dog) walk() {
+	fmt.Println("My name is ", d.first, "and i'm walking")
+}
+
+func (d *dog) run() {
+	d.first = "Rover"
+	fmt.Println("My name is ", d.first, "and i'm runing `")
+}
+
+func main() {
+	d1 := dog{"henery"}
+	d1.walk()
+	d1.run()
+	d2 := &dog{"Padget"}
+	d2.walk()
+	d2.run()
+}
