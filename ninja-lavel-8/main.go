132cf62983b6aa62dd1a1fc39c5c3e72f5d8820c
Nahi002 <nahomdebele002@gmail.com>
2024-07-11 22:19:23 +0300

    added unmarshing ninja 8

--- ninja-lavel-8/main.go
+++ ninja-lavel-8/main.go
@@ -13,6 +13,13 @@ type user struct {
 	Age   int
 }
 
+type person struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
 func main() {
 	u1 := user{
 		First: "Nah",
@@ -30,4 +37,14 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(bs))
+
+	//-----------------Unmarshal
+
+	jsonData := `[{"First":"Nah","Age":25 , "Sayings":["Love me or hate me i dont care , if you are mine you will be mine"]},{"First":"Mercy","Age":23 , "Sayings":["I love you , but you are not giving me the perfect conditions to lvoe you and accept your love , but i have faith in you one day we willbe maried , when you changed your approach."]}]`
+	var people []person
+	err = json.Unmarshal([]byte(jsonData), &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(people)
 }
