4776bdf01084fa7e55ac9b0f66d06153af2ef0cc
Nahi002 <nahomdebele002@gmail.com>
2024-07-11 22:23:54 +0300

    Encoding code

---
+++ encoding/main.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+Encode to json the []user sending the results to Stdout.Hint you will need to use
+json.NewEncoder(os.StdOut).encode(v interface{})
+*/
+
+type user struct {
+}
+
+func main() {}
