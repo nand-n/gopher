9dba70a334ac6f92654e9b687acb11f8eccb310f
Nahi002 <nahomdebele002@gmail.com>
2024-07-11 22:06:16 +0300

    Marshal  nina 8

--- ninja-lavel-8/main.go
+++ ninja-lavel-8/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 /*
-Start
+Marshal and unmarshal it
 */
+type user struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := user{
+		First: "Nah",
+		Age:   25,
+	}
+	u2 := user{
+		First: "Mercy",
+		Age:   23,
+	}
+	users := []user{u1, u2}
+
+	fmt.Println(users)
+	bs, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
