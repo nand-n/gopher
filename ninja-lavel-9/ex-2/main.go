8057426dfe8597acf82ed750290ad11a43aa6885
Nahi002 <nahomdebele002@gmail.com>
2024-07-13 23:31:44 +0300

    you are doing greate in learing

--- ninja-lavel-9/ex-2/main.go
+++ ninja-lavel-9/ex-2/main.go
@@ -1,1 +1,43 @@
 package main
+
+import "fmt"
+
+/*
+This exercise will reinfore our understanding of mehtod sets
+	- Create a type person struct
+	- attach a method speack to type pointer to person
+		- *person
+	- create type human interface
+		- to implicitly implement the interface , a human must have the speak mehtod
+	- create func saySomthing
+		- have it take in a human as parameter
+		- have it call the speak method
+	- show the ff in you r code
+		- you CAN pass type *person into saySomething
+		- you CANNOT pass the type person into saySomething
+
+
+*/
+
+type person struct {
+	Name string
+}
+type human interface {
+	speak()
+}
+
+func (p *person) speak() {
+	fmt.Println("helow")
+}
+
+func saySomthing(h human) {
+	h.speak()
+}
+
+func main() {
+	p := person{
+		Name: "James",
+	}
+
+	saySomthing(&p)
+}
