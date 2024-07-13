94b9f1dbeafc1678a4f3e98531c6bcc8472e6d68
Nahi002 <nahomdebele002@gmail.com>
2024-07-13 22:22:47 +0300

    racve function

---
+++ race-condition/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs : ", runtime.NumCPU())
+	fmt.Println("Goroutienes : ", runtime.NumGoroutine())
+
+	counter := 0
+	const gs = 100
+
+	var wg sync.WaitGroup
+	wg.Add(gs)
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			// time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutienes  in the loop: ", runtime.NumGoroutine())
+
+	}
+
+	wg.Wait()
+	fmt.Println("Goroutienes : ", runtime.NumGoroutine())
+	fmt.Println("Counter : ", counter)
+
+}
