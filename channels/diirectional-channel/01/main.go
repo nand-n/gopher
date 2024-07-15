df5cf43ced5fe4d150e961fc7266ace0e2a728a1
Nahi002 <nahomdebele002@gmail.com>
2024-07-15 22:56:46 +0300

    directional chanle code

--- channels/diirectional-channel/01/main.go
+++ channels/diirectional-channel/01/main.go
@@ -13,4 +13,13 @@ func main() {
 	fmt.Println("-------")
 	fmt.Printf("%T\n", c)
 
+	cr := make(<-chan int) //reciever
+	cs := make(chan<- int) // send
+
+	fmt.Println(<-cr)
+	fmt.Println(cr)
+	fmt.Println("-------")
+	fmt.Printf("%T\n", cr)
+	fmt.Printf("%T\n", cs)
+
 }
