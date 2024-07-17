5423a9da26f1d16c366a0dfed0218a9b7d9ed79c
Nahi002 <nahomdebele002@gmail.com>
2024-07-17 22:18:59 +0300

    context wtih cancel  code

--- channels/08-context/main.go
+++ channels/08-context/main.go
@@ -12,4 +12,10 @@ func main() {
 	fmt.Printf("Context type:\t%T\n", ctx)
 	fmt.Println("-------------")
 
+	ctx, _ = context.WithCancel()
+	fmt.Println("Context:\t", ctx)
+	fmt.Println("Context err:\t", ctx.Err())
+	fmt.Printf("Context type:\t%T\n", ctx)
+	fmt.Println("-------------")
+
 }
