e9d1c14983a32af1b607dca15360c47e9acb61fb
Nahi002 <nahomdebele002@gmail.com>
2024-07-18 22:20:17 +0300

    created files

---
+++ errorHanling/02/names.js
@@ -0,0 +1,1 @@
+const n = [1,2,3,4]; console.log(n.map(a=> a))
---
+++ errorHanling/02/names.tsx
@@ -0,0 +1,6 @@
+const n = [1, 2, 3, 4];
+console.log(n.map(a));
+
+function a(value: number, index: number, array: number[]): unknown {
+  throw new Error("Function not implemented.");
+}
